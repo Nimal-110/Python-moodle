dc01424d80b22b403df9bc0c309ab2865da0ac04
Nimal-110 <231501110@rajalakshmi.edu.in>
2024-06-15 14:15:18 +0530

    Week-9.03.py

--- Week-9/Week-9.03.py
+++ Week-9/Week-9.03.py
@@ -1,7 +1,7 @@
 Winner of Election
 
 Given an array of names of candidates in an election. A candidate name in the array represents a vote cast to the candidate. Print the name of candidates received Max vote. If there is tie, print a lexicographically smaller name.
-
+ 
 Examples: 
 
 Input :  votes[] = {"john", "johnny", "jackie",
