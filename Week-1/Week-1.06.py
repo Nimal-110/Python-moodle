f3890f413b007fbb516ce59c90a3a040ca2d4ab9
Nimal-110 <231501110@rajalakshmi.edu.in>
2024-06-15 13:44:25 +0530

    Week-1.06.py

--- Week-1/Week-1.06.py
+++ Week-1/Week-1.06.py
@@ -2,7 +2,7 @@ Carpenter
 
 Justin is a carpenter who works on an hourly basis. He works in a company where he is paid Rs 50 for an hour on weekdays and Rs 80 for an hour on weekends. He works 10 hrs more on weekdays than weekends. If the salary paid for him is given, write a program to find the number of hours he has worked on weekdays and weekends.
 
-Hint:
+Hint: 
 
 If the final result(hrs) are in -ve convert that to +ve using abs() function
 
