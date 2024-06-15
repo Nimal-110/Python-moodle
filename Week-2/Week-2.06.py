3599a44c0e6991a8457825dda201d4c4150923a2
Nimal-110 <231501110@rajalakshmi.edu.in>
2024-06-15 13:54:21 +0530

    Week-2.06.py

--- Week-2/Week-2.06.py
+++ Week-2/Week-2.06.py
@@ -4,7 +4,7 @@ A team from the Rotract club had planned to conduct a rally to create awareness 
 
  Write a program and feed it to the system to find whether a person is eligible or not.
 
-
+ 
 
 Input Format:
 
